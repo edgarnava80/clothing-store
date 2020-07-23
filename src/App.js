2da307fbc9795bd124a953cc5f5c598fcbe07f2b
edgarnava80 <edgarnr@hotmail.com>
2020-07-22 22:11:39 -0500

    Sing in form, input, button components added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
+import LoginPage from "./pages/login/login.component"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </div>
   )
---
+++ src/pages/login/login.component.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import SignIn from "../../components/sign-in/sign-in.component"
+import "./login.styles.scss"
+
+const LoginPage = () => (
+  <div className="log-in">
+    <SignIn />
+  </div>
+)
+export default LoginPage
