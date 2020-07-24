fa9be5eaa9344891de69cccf3699a835257c94bd
edgarnava80 <edgarnr@hotmail.com>
2020-07-24 12:56:14 -0500

    Storage of users in firebase and email authentication implemented

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import SignIn from "../../components/sign-in/sign-in.component"
+import SignUp from "../../components/sign-up/sign-up.component"
 import "./login.styles.scss"
 
 const LoginPage = () => (
-  <div className="log-in">
+  <div className="login">
     <SignIn />
+    <SignUp />
   </div>
 )
 export default LoginPage
