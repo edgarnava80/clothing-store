8cb2cfde4390e3437fea0d1a3e0222a8dba4b0e5
edgarnava80 <edgarnr@hotmail.com>
2020-07-26 18:17:54 -0500

    Added memorization with reselect memorization to app, cartIcon and cartDropdown components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
@@ -8,6 +9,7 @@ import Header from "./components/header/header.component"
 import LoginPage from "./pages/login/login.component"
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 import { setCurrentUser } from "./redux/user/user.actions"
+import { selectCurrentUser } from "./redux/user/user.selectors"
 
 class App extends React.Component {
   // constructor() {
@@ -53,8 +55,8 @@ class App extends React.Component {
     )
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchoProps = dispatch => ({
